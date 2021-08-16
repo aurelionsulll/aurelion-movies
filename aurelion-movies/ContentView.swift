22e45e45ff98ff2029b6e6d0cce0a28969add017
aurelionsulll <mennanamehdi@gmail.com>
2021-08-16 18:29:54 +0100

    Initial Commit

---
+++ aurelion-movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aurelion-movies
+//
+//  Created by mehdi on 16/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aurelion-movies/aurelion_moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  aurelion_moviesApp.swift
+//  aurelion-movies
+//
+//  Created by mehdi on 16/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct aurelion_moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
