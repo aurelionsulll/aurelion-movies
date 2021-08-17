896ad9b253567e0ada2c0c5426d3dca3b0a60baa
aurelionsulll <mennanamehdi@gmail.com>
2021-08-17 01:27:59 +0100

    network manager

---
+++ aurelion-movies/Networking/NetworkManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkManager.swift
+//  aurelion-movies
+//
+//  Created by mehdi on 17/08/2021.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkManager<T: Codable> {
+    static func fetch(from urlString: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        
+        AF.request(urlString).responseDecodable(of: T.self) { (resp) in
+            if resp.error != nil {
+                completion(.failure(.invalidResponse))
+                print(resp.error!)
+                return
+            }
+            
+            if let payload = resp.value {
+                completion(.success(payload))
+                return
+            }
+            
+            completion(.failure(.nilResponse))
+        }
+        
+    }
+    
+    enum NetworkError: Error {
+        case invalidResponse
+        case nilResponse
+    }
+}
